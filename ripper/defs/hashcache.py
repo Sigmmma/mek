00564f71de88c019a7ab088d5aac5b5c98da8143
Moses <Moses@localhost>
2017-04-13 16:10:12 -0400

    Renamed tag lists in the ripper

--- ripper/defs/hashcache.py
+++ ripper/defs/hashcache.py
@@ -1,33 +1,39 @@
 from supyr_struct.defs.tag_def import *
+try:
+    from binilla.field_widgets import TextFrame
+except Exception:
+    TextFrame = None
 
-def get(): return hash_cache_def
+def get():
+    return hash_cache_def
 
 hash_cache_header = Struct("header",
-    LUInt32("id", DEFAULT='hsah'),
-    LUInt32("version", DEFAULT=3),
+    UInt32("id", DEFAULT='hsah', EDITABLE=False),
+    UInt32("version", DEFAULT=3, EDITABLE=False),
 
-    LUInt32("hashcount"),
-    LUInt16("hashsize"),
-    LUInt16("namelen"),
-    LUInt32("descriptionlen"),
+    UInt32("hashcount", EDITABLE=False),
+    UInt16("hashsize", EDITABLE=False),
+    UInt16("namelen", VISIBLE=False),
+    UInt32("descriptionlen", VISIBLE=False),
 
     Pad(76), #ROOM FOR ADDITIONAL DATA
 
-    StrLatin1("hashmethod", SIZE=32),
+    StrLatin1("hashmethod", SIZE=32, EDITABLE=False),
     SIZE=128
     )
                           
 hash_desc = Container("hash",
     BytesRaw("hash", SIZE="...header.hashsize"),
-    LSInt16("value size"),
+    SInt16("value size", VISIBLE=False, EDITABLE=False),
     StrUtf8("value", SIZE=".value_size")
     )
 
 hash_cache_def = TagDef("hashcache",
     hash_cache_header,
     StrUtf8("cache_name", SIZE=".header.namelen"),
-    StrUtf8("cache_description", SIZE=".header.descriptionlen"),
+    StrUtf8("cache_description",
+        SIZE=".header.descriptionlen", WIDGET=TextFrame),
     Array("cache", SIZE=".header.hashcount", SUB_STRUCT=hash_desc),
 
-    ext=".hashcache"
+    ext=".hashcache", endian="<"
     )
