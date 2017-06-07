c5aa8fd8d6a5e9f0af3f51080df45733bd8701fd
Moses <Moses@localhost>
2017-06-07 00:46:21 -0400

    Removed stubbs model compiler as it is obsolete with Refinery out Created tools to convert stubbs antr and soso tags to halo ones     Removed stubbs antr and soso compilers as this makes them obsolete Changes to the meta_to_tag_data function     Multiplies grenade velocity by 30 in actv     Divides wobble function period in cdmg and jpt! camera_shaking by 30     Fixed incorrect scale when multiplying corona rotation in lens     Removed the clearing of chunks of data in sbsp leaves and breakable surfaces     Properly pads compressed and uncompressed sbsp lightmap vertices up to         the size they would be expected to be if they were valid.         Change this in the future to recalculate the other type.     Changed it so scenario duplicates are renamed in reverse order(last is unchanged)     Divides cutscene times by 30

---
+++ Converter-Stubbs_Antr_to_Halo_Antr.py
@@ -0,0 +1,176 @@
+import os, struct, supyr_struct
+
+from time import time
+from tkinter import *
+from tkinter.filedialog import askdirectory
+from traceback import format_exc
+
+from supyr_struct.defs.constants import fcc, PATHDIV, FieldParseError
+from supyr_struct.defs.block_def import BlockDef
+from reclaimer.hek.defs.antr    import antr_def
+from reclaimer.stubbs.defs.antr import antr_def as stubbs_antr_def
+from reclaimer.common_descs import tag_header_os
+
+
+tag_header_def = BlockDef(tag_header_os)
+
+PATHDIV = PATHDIV
+curr_dir = os.path.abspath(os.curdir) + PATHDIV
+
+
+def convert_antr_tag(antr_path):
+    # make a new tag and load the stubbs one
+    antr_tag = antr_def.build()
+    tagdata  = antr_tag.data.tagdata
+    stubbs_antr_tag = stubbs_antr_def.build(filepath=antr_path)
+    stubbs_tagdata  = stubbs_antr_tag.data.tagdata
+
+    # animation descriptor
+    tagdata.animations.STEPTREE.append()
+    animation = tagdata.animations.STEPTREE[-1]
+
+    animation_desc   = animation.desc
+
+    # copy all reflexives except the units, vehicles, and damages
+    tagdata.objects    = stubbs_tagdata.objects
+    tagdata.weapons    = stubbs_tagdata.weapons
+    tagdata.devices    = stubbs_tagdata.devices
+    tagdata.fp_animations         = stubbs_tagdata.fp_animations
+    tagdata.sound_references      = stubbs_tagdata.effect_references
+    tagdata.limp_body_node_radius = stubbs_tagdata.limp_body_node_radius
+    tagdata.flags      = stubbs_tagdata.flags
+    tagdata.nodes      = stubbs_tagdata.nodes
+    tagdata.animations = stubbs_tagdata.animations
+
+    # copy everything in the unit block except the stubbs stuff
+    units        = tagdata.units.STEPTREE
+    stubbs_units = stubbs_tagdata.units.STEPTREE
+    units.extend(len(stubbs_units))
+    for i in range(len(units)):
+        unit = units[i]
+        stubbs_unit = stubbs_units[i]
+        for j in range(11):
+            unit[j] = stubbs_unit[j]
+
+        unit_weapons         = unit.weapons.STEPTREE
+        stubbs_unit_weapons  = stubbs_unit.weapons.STEPTREE
+        unit_weapons.extend(len(stubbs_unit_weapons))
+        for j in range(len(unit_weapons)):
+            unit_weapon = unit_weapons[j]
+            stubbs_unit_weapon = stubbs_unit_weapons[j]
+
+            for k in range(11):
+                unit_weapon[k] = stubbs_unit_weapon[k]
+
+            unit_weapon[12] = stubbs_unit_weapon[12]
+            unit_weapon[13] = stubbs_unit_weapon[13]
+
+    # copy everything in the vehicle block except the stubbs stuff
+    vehicles        = tagdata.vehicles.STEPTREE
+    stubbs_vehicles = stubbs_tagdata.vehicles.STEPTREE
+    vehicles.extend(len(stubbs_vehicles))
+    for i in range(len(vehicles)):
+        vehicle = vehicles[i]
+        stubbs_vehicle = stubbs_vehicles[i]
+        for j in range(8):
+            vehicle[j] = stubbs_vehicle[j]
+
+        vehicle[8] = stubbs_vehicle[9]
+        vehicle[9] = stubbs_vehicle[10]
+
+
+    #swap the animation descriptors
+    for animation in tagdata.animations.STEPTREE:
+        animation.desc = animation_desc
+
+
+    # replace the filepath
+    antr_tag.filepath = stubbs_antr_tag.filepath
+    return antr_tag
+
+
+class StubbsAntrConverter(Tk):
+    def __init__(self, *args, **kwargs):
+        Tk.__init__(self, *args, **kwargs)
+
+        self.title("Stubbs antr tag converter v1.0")
+        self.geometry("400x80+0+0")
+        self.resizable(0, 0)
+
+        self.tags_dir = StringVar(self)
+        self.tags_dir.set(curr_dir + 'tags' + PATHDIV)
+
+        # make the frames
+        self.tags_dir_frame = LabelFrame(
+            self, text="Directory of model_animation tags")
+
+        # add the filepath boxes
+        self.tags_dir_entry = Entry(
+            self.tags_dir_frame, textvariable=self.tags_dir)
+        self.tags_dir_entry.config(width=55, state=DISABLED)
+
+        # add the buttons
+        self.convert_btn = Button(
+            self, text="Convert", width=15, command=self.convert)
+        self.tags_dir_browse_btn = Button(
+            self.tags_dir_frame, text="Browse",
+            width=6, command=self.tags_dir_browse)
+
+        # pack everything
+        self.tags_dir_entry.pack(expand=True, fill='x', side='left')
+        self.tags_dir_browse_btn.pack(fill='x', side='left')
+        self.tags_dir_frame.pack(expand=True, fill='both')
+        self.convert_btn.pack(fill='both', padx=5, pady=5)
+
+    def destroy(self):
+        Tk.destroy(self)
+        #raise SystemExit(0)
+        os._exit(0)
+        
+    def tags_dir_browse(self):
+        dirpath = askdirectory(initialdir=self.tags_dir.get())
+        if dirpath:
+            self.tags_dir.set(dirpath)
+
+    def convert(self):
+        print('Converting model_animations\n')
+        start = time()
+        tags_dir = self.tags_dir.get()
+
+        if not tags_dir.endswith(PATHDIV):
+            tags_dir += PATHDIV
+
+        for root, dirs, files in os.walk(tags_dir):
+            if not root.endswith(PATHDIV):
+                root += PATHDIV
+
+            for filename in files:
+                filepath = root + filename
+                if not os.path.splitext(filename)[-1].lower() == '.model_animations':
+                    continue
+
+                try:
+                    blam_header = tag_header_def.build(filepath=filepath)
+                    if blam_header.version != 5:
+                        continue
+
+                    print(filepath.split(tags_dir)[-1])
+
+                    tag = convert_antr_tag(filepath)
+                    if tag is None:
+                        continue
+                except Exception:
+                    print(format_exc())
+                    print("Could not convert:   %s" % filepath)
+                    continue
+
+                tag.serialize(temp=False, backup=False, int_test=False)
+        print('\nFinished. Took %s seconds' % (time() - start))
+
+try:
+    converter = StubbsAntrConverter()
+    converter.mainloop()
+except Exception:
+    print(format_exc())
+    input()
+
---
+++ Converter-Stubbs_Soso_to_Halo_OS_Soso.py
@@ -0,0 +1,135 @@
+import os, struct
+
+from time import time
+from tkinter import *
+from tkinter.filedialog import askdirectory
+from traceback import format_exc
+
+from supyr_struct.defs.constants import fcc, PATHDIV, FieldParseError
+from supyr_struct.defs.block_def import BlockDef
+from reclaimer.os_v3_hek.defs.soso import soso_def
+from reclaimer.stubbs.defs.soso    import soso_def as stubbs_soso_def
+from reclaimer.common_descs import tag_header_os
+
+
+tag_header_def = BlockDef(tag_header_os)
+
+PATHDIV = PATHDIV
+curr_dir = os.path.abspath(os.curdir) + PATHDIV
+
+
+def convert_soso_tag(soso_path):
+    # make a new tag and load the stubbs one
+    soso_tag   = soso_def.build()
+    soso_attrs = soso_tag.data.tagdata.soso_attrs
+    stubbs_soso_tag   = stubbs_soso_def.build(filepath=soso_path)
+    stubbs_soso_attrs = stubbs_soso_tag.data.tagdata.soso_attrs
+
+    soso_tag.data.tagdata.shdr_attrs = stubbs_soso_tag.data.tagdata.shdr_attrs
+
+    soso_attrs.model_shader          = stubbs_soso_attrs.model_shader
+    soso_attrs.color_change_source   = stubbs_soso_attrs.color_change_source
+    soso_attrs.self_illumination     = stubbs_soso_attrs.self_illumination
+    soso_attrs.maps                  = stubbs_soso_attrs.maps
+    soso_attrs.texture_scrolling     = stubbs_soso_attrs.texture_scrolling
+    soso_attrs.reflection_properties = stubbs_soso_attrs.reflection_properties
+
+    # make a new shader extension for the bump map
+    soso_ext = soso_attrs.os_shader_model_ext.STEPTREE
+    soso_ext.extend(1)
+    b = soso_ext[-1]
+    bump_props = stubbs_soso_attrs.bump_properties
+    b.base_normal_coefficient  = bump_props.bump_scale
+    b.base_normal_map.filepath = bump_props.bump_map.filepath
+
+    # replace the filepath
+    soso_tag.filepath = stubbs_soso_tag.filepath
+
+    return soso_tag
+
+
+class StubbsSosoConverter(Tk):
+    def __init__(self, *args, **kwargs):
+        Tk.__init__(self, *args, **kwargs)
+
+        self.title("Stubbs soso tag converter v1.0")
+        self.geometry("400x80+0+0")
+        self.resizable(0, 0)
+
+        self.tags_dir = StringVar(self)
+        self.tags_dir.set(curr_dir + 'tags' + PATHDIV)
+
+        # make the frames
+        self.tags_dir_frame = LabelFrame(
+            self, text="Directory of shader_model tags")
+
+        # add the filepath boxes
+        self.tags_dir_entry = Entry(
+            self.tags_dir_frame, textvariable=self.tags_dir)
+        self.tags_dir_entry.config(width=55, state=DISABLED)
+
+        # add the buttons
+        self.convert_btn = Button(
+            self, text="Convert", width=15, command=self.convert)
+        self.tags_dir_browse_btn = Button(
+            self.tags_dir_frame, text="Browse",
+            width=6, command=self.tags_dir_browse)
+
+        # pack everything
+        self.tags_dir_entry.pack(expand=True, fill='x', side='left')
+        self.tags_dir_browse_btn.pack(fill='x', side='left')
+        self.tags_dir_frame.pack(expand=True, fill='both')
+        self.convert_btn.pack(fill='both', padx=5, pady=5)
+
+    def destroy(self):
+        Tk.destroy(self)
+        #raise SystemExit(0)
+        os._exit(0)
+        
+    def tags_dir_browse(self):
+        dirpath = askdirectory(initialdir=self.tags_dir.get())
+        if dirpath:
+            self.tags_dir.set(dirpath)
+
+    def convert(self):
+        print('Converting shader_models\n')
+        start = time()
+        tags_dir = self.tags_dir.get()
+
+        if not tags_dir.endswith(PATHDIV):
+            tags_dir += PATHDIV
+
+        for root, dirs, files in os.walk(tags_dir):
+            if not root.endswith(PATHDIV):
+                root += PATHDIV
+
+            for filename in files:
+                filepath = root + filename
+                if not os.path.splitext(filename)[-1].lower() == '.shader_model':
+                    continue
+
+                try:
+                    blam_header = tag_header_def.build(filepath=filepath)
+                    if blam_header.version != 3:
+                        continue
+
+                    print(filepath.split(tags_dir)[-1])
+
+                    tag = convert_soso_tag(filepath)
+                    if tag is None:
+                        continue
+                except Exception:
+                    print(format_exc())
+                    print("Could not convert:   %s" % filepath)
+                    continue
+
+                tag.serialize(temp=False, backup=False, int_test=False)
+        print('\nFinished. Took %s seconds' % (time() - start))
+
+try:
+    converter = StubbsSosoConverter()
+    converter.mainloop()
+except Exception:
+    print(format_exc())
+    input()
+
