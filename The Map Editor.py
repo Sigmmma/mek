25838da33f39753d32036404e03b1fe20497d7d7
Moses_of_Egypt <29311834+MosesofEgypt@users.noreply.github.com>
2017-07-15 14:47:32 -0400

    Moved fps changers into useless folder(Michelle is working on a runtime tag     fps changer that will make this method obsolete) Moved most of refinery into refinery_core/main.py Implemented most of zteam map deprotection. Reaches around 80% coverage.

--- The Map Editor.py
+++ The Map Editor.py
@@ -1,19 +1,18 @@
 print("The Map Editor is warming up...")
-from Refinery import *
+
+try:
+    from traceback import format_exc
+    from refinery_core.main import *
 
 
-class TheMapEditor(Refinery):
+    class TheMapEditor(Refinery):
 
-    def __init__(self, *args, **kwargs):
-        Refinery.__init__(self, *args, **kwargs)
-        self.title("The Map Editor" +
-                   self.title().lower().split('refinery')[-1])
-
-
-if __name__ == "__main__":
-    try:
-        extractor = TheMapEditor()
-        extractor.mainloop()
-    except Exception:
-        print(format_exc())
-        input()
+        def __init__(self, *args, **kwargs):
+            Refinery.__init__(self, *args, **kwargs)
+            self.title("The Map Editor" +
+                       self.title().lower().split('refinery')[-1])
+    extractor = TheMapEditor()
+    extractor.mainloop()
+except Exception:
+    print(format_exc())
+    input()
