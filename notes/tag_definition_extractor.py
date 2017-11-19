789e9a697b79981efc0d771b519e51eeccc57ec2
Moses_of_Egypt <Moses_of_Egypt@localhost>
2017-11-19 09:18:21 -0500

    hdd failing. saving work.

--- notes/tag_definition_extractor.py
+++ notes/tag_definition_extractor.py
@@ -7,6 +7,27 @@ from traceback import format_exc
 from supyr_struct.tag import Tag
 
 
+def find_enums(block, next_blocks):
+    typ = block.TYPE
+    if "enum" in typ.name.lower():
+        next_blocks.append(block)
+        return
+
+    if typ.is_array:
+        if block.get_desc('TYPE', 'SUB_STRUCT').is_block:
+            if len(block) == 0:
+                block.append()
+            find_enums(block[0], next_blocks)
+    elif typ.is_container or typ.is_struct:
+        for i in range(len(block)):
+            if block.get_desc('TYPE', i).is_block:
+                find_enums(block[i], next_blocks)
+
+    if (hasattr(block, "STEPTREE") and
+            block.get_desc('TYPE', 'STEPTREE').is_block):
+        find_enums(block.STEPTREE, next_blocks)
+
+
 def find_next_blocks(block, next_blocks):
     typ = block.TYPE
     if typ in (Reflexive, H2MetaReflexive, H2Reflexive):
@@ -19,7 +40,6 @@ def find_next_blocks(block, next_blocks):
             if len(block) == 0:
                 block.append()
             find_next_blocks(block[0], next_blocks)
-
     elif typ.is_container or typ.is_struct:
         for i in range(len(block)):
             if block.get_desc('TYPE', i).is_block:
@@ -36,12 +56,19 @@ def print_tag_def(tag_def):
 
     # write the tag definition to a file
     with open("%s_def.txt" % tag_def.def_id, "w") as f:
+        tagdata = new_tag
+        def_id = tag_def.def_id
         if isinstance(new_tag, Tag):
-            blocks = [new_tag.data.blam_header, new_tag.data.tagdata]
-            f.write("%s (%s)\n\n" % (tag_def.def_id, tag_def.ext[1:]))
+            tagdata = new_tag.data.tagdata
+            blocks = [new_tag.data.blam_header, tagdata]
+            f.write("%s (%s)\n\n" % (def_id, tag_def.ext[1:]))
         else:
-            blocks = [new_tag]
-            f.write("%s\n\n" % tag_def.def_id)
+            blocks = [tagdata]
+            f.write("%s\n\n" % def_id)
+
+        f.write("#"*60 + "\n")
+        f.write("#" + (" "*20) + "STRUCTURES\n")
+        f.write("\n\n")
 
         # loop over each block until they are exhausted
         while blocks:
@@ -55,14 +82,63 @@ def print_tag_def(tag_def):
                 f.write("\n")
                 f.write("#"*60 + "\n")
                 f.write("#" + (" "*20) + block.NAME + "\n")
-                f.write("#"*60 + "\n")
                 f.write("\n")
                 f.write(block.pprint(show=("name", "type", "offset",
-                                           "steptrees", "flags", "size")))
+                                           "flags", "size")).\
+                        replace("[", "{").replace("]", "}"))
 
                 find_next_blocks(block, new_blocks)
 
             blocks = new_blocks
+
+        f.write("\n\n")
+        f.write("#"*60 + "\n")
+        f.write("#" + (" "*20) + "ENUMERATORS\n")
+        f.write("\n\n")
+
+        # loop over each block until they are exhausted
+        enums = []
+        find_enums(tagdata, enums)
+        i = j = 0
+        enum_strs = []
+        enum_strs_map = {}
+        enum_names = []
+        for enum in enums:
+            name = ""
+            b = enum
+            while hasattr(b, 'NAME') and hasattr(b, 'parent'):
+                if not (b.TYPE.is_array or b.NAME in ("tagdata", def_id) or
+                        b.TYPE in (Reflexive, H2MetaReflexive, H2Reflexive)):
+                    name = "%s.%s" % (b.NAME, name)
+                b = b.parent
+            name = name.rstrip(".")
+
+            if not name:
+                name = "UNNAMED_ENUM_%s" % i
+                i += 1
+
+            desc = enum.desc
+            enum_str = "%s" + ("%s {\n" % enum.TYPE.name)
+            for k in range(enum.ENTRIES):
+                enum_str += "    %s = %s,\n" % (desc[k]['NAME'],
+                                                desc[k]['VALUE'])
+            enum_str += "    }\n\n"
+
+            if enum_str in enum_strs_map:
+                enum_names[enum_strs_map[enum_str]].append(name)
+                continue
+
+            enum_strs_map[enum_str] = j
+            enum_strs.append(enum_str)
+            enum_names.append([name])
+            j += 1
+
+        for i in range(len(enum_names)):
+            name = ""
+            names = enum_names[i]
+            for j in range(len(names)):
+                name = "%s = %s" % (names[j], name)
+            f.write(enum_strs[i] % name)
 
 tag_defs = {}
 
